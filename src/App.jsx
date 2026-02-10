4007b0ea3405e8a98559397560051bc43672ec4d
Srilatha <msrilatha859@gmail.com>
2026-02-10 14:37:52 +0530

    initial api commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {useEffect,useState} from "react";
+function App(){
+  const [data,setData] = useState([])
+  useEffect(() => {
+    fetch("https://dummyjson.com/recipes")
+    .then((res) => res.json())
+    .then((ans) => setData(ans))
+    .catch((err) => console.log(err))
+    }, [])
+  
+  return(
+    <>
+      <header className="bg-gray-600 text-white text-3xl text-center py-3">Basic Api</header>
+      <main className="my-3 gap-3 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {data.recipes &&
+        data.recipes.map((r) => (
+          <div className="p-5 flex flex-col justify-between rounded-2xl shadow shadow-amber-600">
+            <img src={r.image}
+            alt={r.name} />
+            <h2>{r.name}</h2>
+            <p>Rating:{r.rating}/5</p>
+          </div>
+        )
+        )
+        }
+      </main>
+      <footer className="bg-gray-600 text-white text-2xl text-center py-2">Application made with 💖 by Srilatha</footer>
+    </>
+  )
+}
+export default App;
